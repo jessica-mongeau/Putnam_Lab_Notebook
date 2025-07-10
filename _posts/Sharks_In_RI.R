66c2888355606b0ec2a97557a510a4ad6f5c7869
jessica-mongeau <jessica_mongeau@uri.edu>
2025-07-10 08:19:22 -0400

    Create Sharks_In_RI.R

---
+++ _posts/Sharks_In_RI.R
@@ -0,0 +1,27 @@
+---
+layout: post
+title: Shark Species Found in Rhode Island
+date: '07-10-2025'
+categories: Fun Posts
+tags: [Fun Post, Sharks]
+---
+  
+#Shark Species in Rhode Island!
+  *This is a short fun post about 2 different types of sharks found in Rhode Island either year round or occasionally during the summers!*
+  
+  **What Are Sharks?**
+  Sharks are marine animals are classified as Elasmobranchs, characterized for having cartilaginous skeletons instead of bone. Skates and rays also fall under the Elasmobranch subclass.
+
+### 1. _Mustelus canis_- Smooth Dogfish
+- Smooth dogfish have many small rounded teeth, perfect for crushing through hard shells. 
+- They are viviparous meaning they give birth to live young. 
+- They are the largest species of shark found in the Narragansett Bay year round.
+- Commonly found as bycatch.
+![Smooth Dogfish](https://github.com/jessica-mongeau/Mongeau_Putnam_Lab_Notebook/blob/master/images/SmoothDogfish.jpg)
+
+### 2. _Isurus oxyrinchus_- Shortfin Mako
+- Known as the fastst shark in the world, being able to reach speeds of 45 miles per hour (74 kilometers per hour).
+- The shortfin mako is an endothermic species, meaning it can generate its own heat regardless of external termperatures.
+- They are a highly mirgatory species. 
+- They are viviparous meaning they give birth to live young.
+![Shortfin Mako](https://github.com/jessica-mongeau/Mongeau_Putnam_Lab_Notebook/blob/master/images/ShortfinMako.jpg)
